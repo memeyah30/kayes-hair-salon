cce0abf76d6ee40ecbe109f55cfd3e33e4591da3
memeyah30 <sapanruffa675@gmail.com>
2026-05-08 01:17:59 +0800

    fix: add missing useLocation import in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
